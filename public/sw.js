d758c9290d9dcc70c9c0b4b75a4f639af6117948
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-24 00:48:16 +0930

    Polish Apple Fluid surfaces and floating navigation

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* FuelMate Service Worker - app-shell cache for offline install */
-const CACHE_NAME = 'fuelmate-cache-v27';
+const CACHE_NAME = 'fuelmate-cache-v28';
 
 function urlFor(path) {
   return new URL(path, self.registration.scope).toString();
--- src/core/version.js
+++ src/core/version.js
@@ -1,5 +1,5 @@
 (function exposeFuelMateVersion(global) {
   global.FuelMateVersion = Object.freeze({
-    current: '4.3.0',
+    current: '4.3.1',
   });
 })(globalThis);
